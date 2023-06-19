1fc0230a409976c948855533e5aa9b29f4fb9ef1
Jake Herman <jake.t.herman@gmail.com>
2023-06-19 17:33:29 +0100

    removed keys

--- src/Spotify/Spotify.js
+++ src/Spotify/Spotify.js
@@ -1,5 +1,4 @@
-const client_id = 'd8fafe2aa07f42f38aa1ece132b351d3';
-const client_secret = '931924c99a8c477fa6d44f3077978cea';
+
 // Spotify redirect URL
 const redirectUri = 'http://localhost:3000';
 
